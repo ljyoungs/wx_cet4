408f88b37c3165b16226fbd2b4cb137363aac8b7
Psy <Psy@DESKTOP-PEHHAOJ>
2017-01-18 23:27:04 +0800

    11

--- src/main/java/cn/chenmuxin/wx_cet4/App.java
+++ src/main/java/cn/chenmuxin/wx_cet4/App.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
-	//加个注释传上去1
+	//加个注释传上去12
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
