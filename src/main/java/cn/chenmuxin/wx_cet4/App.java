e29ed5ddde67c93260b332384f11d128fcc2c3f0
sy p <sy p>
2017-01-18 23:11:23 +0800

    second

--- src/main/java/cn/chenmuxin/wx_cet4/App.java
+++ src/main/java/cn/chenmuxin/wx_cet4/App.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
+	//加个注释传上去
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
