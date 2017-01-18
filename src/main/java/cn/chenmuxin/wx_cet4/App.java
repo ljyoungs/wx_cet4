c0302f609155cf75d5cc6c8b2e4fa3951d0fc35f
Psy <Psy@DESKTOP-PEHHAOJ>
2017-01-18 23:14:21 +0800

    加注释测试

--- src/main/java/cn/chenmuxin/wx_cet4/App.java
+++ src/main/java/cn/chenmuxin/wx_cet4/App.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App 
 {
-	//加个注释传上去
+	//加个注释传上去1
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
